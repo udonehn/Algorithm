e8b6d6b97cfda145c083a31c4ee6e69e31d3bd23
DongWoo Nam <udonehn@gmail.com>
2023-07-12 13:05:45 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 744 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -0,0 +1,19 @@
+N = int(input())
+
+def hanoi(n, A,B,C):
+
+    if n ==1:
+
+        print(A,C)
+
+    else:
+
+        hanoi(n-1,A,C,B)
+
+        print(A,C)
+
+        hanoi(n-1,B,A,C)
+
+print(2**N-1)
+
+hanoi(N,'1','2','3')
