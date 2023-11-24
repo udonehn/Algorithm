8e4aa5262ee6abbba961b9c04fa8f8477d842628
DongWoo Nam <udonehn@gmail.com>
2023-11-24 23:10:30 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 212 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        int year;
+        Scanner sc = new Scanner(System.in);
+        year = sc.nextInt();
+        System.out.println(year-543);
+    }
+}
