f26f51c88fa0c06e475254ed91c6417f9834e827
DongWoo Nam <udonehn@gmail.com>
2024-05-19 00:11:28 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,16 @@
+for _ in range(int(input())):
+    arr = input()
+    if arr == "Algorithm":
+        print("204")
+    elif arr == "DataAnalysis":
+        print("207")
+    elif arr == "ArtificialIntelligence":
+        print("302")
+    elif arr == "CyberSecurity":
+        print("B101")
+    elif arr == "Network":
+        print("303")
+    elif arr == "Startup":
+        print("501")
+    elif arr == "TestStrategy":
+        print("105")
